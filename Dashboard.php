5fbea2e3417c74377a7f70a6858b07e1c93f17b6
Noval <126931620+NovallFirmansyah@users.noreply.github.com>
2024-04-23 11:14:10 +0700

    Add files via upload

---
+++ Dashboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SIWEB NAVIGASI PENDIDIKAN</title>
+        <link rel="stylesheet" href="Dash.css">
+    </head>
+
+    <body>
+        <h1>iki diisi opo we</h1>
+    </body>
+</html>

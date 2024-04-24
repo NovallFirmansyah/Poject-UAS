3a4563390b833007a4ea0e71e5862172c80ecd4f
NovallFirmansyah <adriannoval2004@gmail.com>
2024-04-24 15:05:30 +0700

    anjay mabar isok ngeslide

--- Dashboard.php
+++ Dashboard.php
@@ -9,7 +9,7 @@
 
     <body>    
     <div class="navbar">
-            <img style="width: 150px;" src="Asset/siweb-navigasi-pendidikan-favicon-color.png">
+            <img style="width: 180px;" src="Asset/logo.png">
             <ul>
                 <li><a href="Beranda.html" class="active">Beranda</a></li>
                 <li><a href="Biodata.html">rencanakan perjalanan</a></li>
@@ -18,7 +18,18 @@
             <img style="width: 50px; margin:5px;" src="Asset/IconTicket.png">
             <img style="width: 50px; margin:5px;" src="Asset/IconSearch.png">
             <img style="width: 50px; margin:5px;" src="Asset/IcomAccount.png">
-    </div>  
+    </div>
+    <div class="mySlides fade">
+        <img src="Asset/Bali.jpg" style="width:100%">
+    </div>
+    <div class="mySlides fade">
+        <img src="Asset/Maluku.jpg" style="width:100%">
+    </div>
+    <!-- Next and previous buttons -->
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    
+    <script src="script.js"></script>
     </body>
 
     <body1>
